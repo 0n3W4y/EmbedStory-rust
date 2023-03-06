459499d353cab1d7462dea696559634218a82fbc
Alexey Power <vulturetm@gmail.com>
2023-03-06 18:31:31 +0300

    sync home

--- src/scenes/game_scenes/tilemap/change_cover_type_handler.rs
+++ src/scenes/game_scenes/tilemap/change_cover_type_handler.rs
@@ -1,17 +1,18 @@
 use bevy::prelude::*;
 
 use super::tile::CoverType;
-use crate::components::tile_component::TileComponent;
+use crate::{components::tile_component::TileComponent, materials::material_manager::MaterialManager};
 
-
+/*
 pub fn change_cover_type_handler(
     mut commands: Commands,
-    mut tile_query: Query<(Entity, &TileComponent, &mut Children),  With<TileComponent>>,
+    mut tile_query: Query<(Entity, &TileComponent, &mut Children),  (Changed<tileComponent>, With<TileComponent>)>,
+    material_manager: Res<MaterialManager>,
 ){
     for(entity, component, children) in tile_query.iter_mut(){
         let texture = match component.cover_type{
             CoverType::None => {
-                let new_texture:Handle<Image> = Default::default();
+                let new_texture:Handle<Image> = material_manager.game_scene.cover_tile.get_image(&component.cover_type, 0);
                 new_texture
             },
             _ => {
@@ -39,19 +40,19 @@ pub fn change_cover_type_handler(
         commands.entity(entity).push_children(&[child]);
     }
 }
+*/
 
-/*
 pub fn change_cover_type_handler(
-    mut commands: Commands,
-    mut tile_query: Query<(&TileComponent, &mut Children), (Changed<TileComponent>, With<TileComponent>)>,
-    mut texture_query: Query<(&mut Handle<Image>, With<TileComponent>)>
+    tile_query: Query<(&TileComponent, &mut Children), (Changed<TileComponent>, With<TileComponent>)>,
+    mut texture_query: Query<(&mut Handle<Image>, With<TileComponent>)>,
+    material_manager: Res<MaterialManager>,
 ){
     for(component, children) in tile_query.iter(){
+        //let new_texture:Handle<Image> = material_manager.game_scene.cover_tile.get_image(&component.cover_type, 0).clone();
         let new_texture:Handle<Image> = Default::default();
-            
-        if let Ok(mut texture) = texture_query.get_mut(*children.first().unwrap()) {
-            texture = &mut new_texture;
+        if let Ok((mut texture, _)) = texture_query.get_mut(*children.first().unwrap()) {
+            *texture = new_texture;
         }
+
     }
 }
-*/
