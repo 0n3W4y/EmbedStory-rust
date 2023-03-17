036a36feca7a757159bac4e2be678e4cd5074b20
0n3W4y <vulturetm@gmail.com>
2023-03-17 16:27:28 +0300

    sync work
    little modify charactor_manager;

--- src/resources/charactor_manager.rs
+++ src/resources/charactor_manager.rs
@@ -1,8 +1,10 @@
 use serde::{Serialize, Deserialize};
 
-use super::{scene_data::objects::charactor::{RaceType, CharactorType, Charactor, RaceDeploy}, deploy::Deploy};
+use super::{scene_data::objects::charactor::{RaceType, CharactorType, Charactor, RaceDeploy, NPCType}, deploy::Deploy};
 use crate::scenes::game_scenes::tilemap::tile::Tile;
 use crate::resources::scene_data::objects::resists::Resist;
+use crate::resources::scene_data::objects::charactor::MonsterType;
+use crate::resources::scene_data::objects::charactor::CompanionType;
 
 #[derive(Default, Clone, Serialize, Deserialize)]
 pub struct CharactorManager{
@@ -12,38 +14,57 @@ pub struct CharactorManager{
 impl CharactorManager {
     pub fn create_player(&mut self, deploy: &Deploy) -> Charactor {
         let race_type = RaceType::Human;
-        let config: RaceDeploy = deploy.charactor_deploy.race_deploy.get_race_config(&race_type);
-        let id = self.create_id();
+        let charactor_type = CharactorType::Player;
+        let race_config: RaceDeploy = deploy.charactor_deploy.race_deploy.get_race_config(&race_type);
+        let mut charactor = self.create_charactor(&charactor_type, &race_type);
+
         let mut resist: Vec<Resist> = vec![];
 
-        let mut charactor = Charactor {
-            charactor_type: CharactorType::Player,
-            race_type,
-            id,
-
-            ..Default::default()
-        };
+        
         return charactor;
     }
-    pub fn create_charactor(&mut self, race_type: &RaceType, charactor_type: &CharactorType, deploy: &Deploy) -> Charactor{
-        let id = self.create_id();
-        let mut charactor = Charactor{
-            race_type: race_type.clone(),
-            charactor_type: charactor_type.clone(),
-            id,
-            ..Default::default()
-        };
+    pub fn create_npc(&mut self, race_type: &RaceType, npc_type: &NPCType, deploy: &Deploy) -> Charactor{
+        let charactor_type = CharactorType::NPC(npc_type.clone());
+        let mut charactor = self.create_charactor(&charactor_type, &race_type);
+        let race_config: RaceDeploy = deploy.charactor_deploy.race_deploy.get_race_config(&race_type);
 
         return charactor;
     }
 
-    pub fn create_charator_on_tile(&mut self, race_type: &RaceType, charactor_type: &CharactorType, deploy: &Deploy, tile: &mut Tile){
-        let mut caharctor = self.create_charactor(race_type, charactor_type, deploy);
+    pub fn create_monster(&mut self, race_type: &RaceType, monster_type: &MonsterType, deploy: &Deploy) -> Charactor {
+        let charactor_type = CharactorType::Monster(monster_type.clone());
+        let mut charactor = self.create_charactor(&charactor_type, &race_type);
+        let race_config: RaceDeploy = deploy.charactor_deploy.race_deploy.get_race_config(&race_type);
+
+        return charactor;
+    }
+
+    pub fn create_compnaion(&mut self, race_type: &RaceType, companion_type: &CompanionType, deploy: &Deploy) -> Charactor {
+        let charactor_type = CharactorType::PlayerCompanion(companion_type.clone());
+        let mut charactor = self.create_charactor(&charactor_type, race_type);
+        let race_config: RaceDeploy = deploy.charactor_deploy.race_deploy.get_race_config(&race_type);
+
+        return charactor;
+    }
+
+    pub fn palce_charator_on_tile(&self, charactor:&mut Charactor, tile: &mut Tile){
+        
     }
 
     fn create_id(&mut self) -> usize {
         let id = self.id;
         self.id += 1;
         return id;
+    }
+
+    fn create_charactor(&mut self, charactor_type: &CharactorType, race_type: &RaceType) -> Charactor {
+        let id = self.create_id();
+        let charactor = Charactor{
+            race_type: race_type.clone(),
+            charactor_type: charactor_type.clone(),
+            id,
+            ..Default::default()
+        };
+        return charactor;
     }
 }
